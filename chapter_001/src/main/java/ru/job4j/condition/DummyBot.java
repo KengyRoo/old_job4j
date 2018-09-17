a1ef0685d3f241f95fd2b7f75c07d8edd8534818
KengyRoo <KengyRoo@mail.ru>
2018-09-18 00:55:28 +0300

    4.1. Глупый бот. [#31323]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+/**
+ * @author Aleksandr Kostyuk (mailto;KengyRoo@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class DummyBot {
+    public String answer(String question){
+        String rsl = "Это ставит меня в тупик. Спросиие другой вопрос.";
+        if ("Привет,Бот".equals(question)){
+            rsl = "Привет, умник";
+        } else if ("Пока".equals(question)){
+            rsl = "До скорой встречи";
+        }
+        return rsl;
+    }
+}
+
