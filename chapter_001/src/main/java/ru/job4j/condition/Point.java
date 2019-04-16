a1595c374d70003908bb3bc0994309d85bd1c18a
KengyRoo <KengyRoo@mail.ru>
2019-04-16 13:56:00 +0300

    1. Рефакторинг - Расстояние между точками. [#122587]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,36 +1,33 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 /**
  * @author Aleksandr Kostyuk (mailto:KengyRoo@mail.ru)
  * @version $Id$
  * @since 0.1
  */
+
+
 public class Point {
+
+
     private int x;
+
+
     private int y;
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
     }
 
-    public double distanceTo(Point that) {
-        return Math.sqrt(
-                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
-        );
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
-    /*public static void main(String[] args) {
-        Point a = new Point(0,1);
-        Point b = new Point(1,5);
-        //Вывод на консоль
-        System.out.println("x1 = " + a.x);
-        System.out.println("y1 = " + a.y);
-        System.out.println("x2 = " + b.x);
-        System.out.println("y2 = " + b.y);
-        double result = a.distanceTo(b);
-        System.out.println("Растояние между точками А и В =" + result);
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
-
-*/
 }
