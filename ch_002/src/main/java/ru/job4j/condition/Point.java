c0a8f022c1e747d7467a6098a3ec7eba7d4e9fdf
KengyRoo <KengyRoo@mail.ru>
2019-06-03 20:49:45 +0300

    2. Рефакторинг - Площадь треугольника. [#122642] upd

--- ch_002/src/main/java/ru/job4j/condition/Point.java
+++ ch_002/src/main/java/ru/job4j/condition/Point.java
@@ -23,7 +23,7 @@ public class Point {
     }
 
     public double distance3d(Point that){
-        return sqrt(sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2)));
+        return Math.sqrt(Math.pow(that.x - this.x, 2) + Math.pow(that.y - this.y, 2) + Math.pow(that.z - this.z, 2));
     }
 
     public double distance(Point that) {
