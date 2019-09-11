be9316b46e6f8db6b2291c78822e8469daf30d0c
KengyRoo <KengyRoo@mail.ru>
2019-09-11 23:00:56 +0300

    1. Конструктор по умолчанию. [#173889]

---
+++ ch_002/src/main/java/ru/job4j/traker/Animal.java
@@ -0,0 +1,8 @@
+package ru.job4j.traker;
+
+public class Animal {
+    public Animal() {
+        super();
+        System.out.println("Animal");
+    }
+}
---
+++ ch_002/src/main/java/ru/job4j/traker/Tiger.java
@@ -0,0 +1,10 @@
+package ru.job4j.traker;
+
+public class Tiger extends Predator {
+    public Tiger(){
+        super();
+        System.out.println("Tiger");
+    }
+}
+
+
