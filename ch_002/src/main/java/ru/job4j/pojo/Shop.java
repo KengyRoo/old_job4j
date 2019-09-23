2268c0e9ffcfb68eb474780454ac5210a2a68bdb
KengyRoo <KengyRoo@mail.ru>
2019-09-23 21:06:03 +0300

    3. Удаление моделей из массива. [#176470]

---
+++ ch_002/src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,55 @@
+package ru.job4j.pojo;
+
+public class Shop {
+
+    public Product[] delete(Product[] products, int index) {
+        products[index] = null;
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
+        }
+        return products;
+    }
+
+    public static void main(String[] args) {
+        Product products[] = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        //products[1] = null;
+        new Shop().delete(products, 1);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем ячейку в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение ячейки с индеском 2");
+        products[1] = products[2];
+        products[2] = null;
+        for (int i=0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+    }
+}
